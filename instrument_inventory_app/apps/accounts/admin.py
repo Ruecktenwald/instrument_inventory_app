4b0423281cf87f03963a512ee701fc6d4d21edd3
Peter <ruecktenwald@gmail.com>
2022-08-27 00:02:22 -0400

    make instrument category and locker models

--- instrument_inventory_app/apps/accounts/admin.py
+++ instrument_inventory_app/apps/accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import UserProfile, UserPersona, UserInterest
+from .models import Instrument, InstrumentCategory, Locker
 
-admin.site.register(UserProfile)
-admin.site.register(UserPersona)
-admin.site.register(UserInterest)
+admin.site.register(Instrument)
+admin.site.register(InstrumentCategory)
+admin.site.register(Locker)
--- instrument_inventory_app/apps/accounts/models.py
+++ instrument_inventory_app/apps/accounts/models.py
@@ -1,35 +1,57 @@
 from django.db import models
-from django.contrib.auth.models import User
+from djchoices import DjangoChoices, ChoiceItem
+#from django.contrib.auth.models import User
 
 
-class UserInterest(models.Model):
+class Locker(models.Model):
+    locker_number = models.CharField(max_length=3, unique=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.locker_number
+
+class InstrumentCategory(models.Model):
+    class classification(DjangoChoices):
+        woodwind_instrument = ChoiceItem()
+        reed_instrument = ChoiceItem()
+        brass_instrument = ChoiceItem()
+        percussion_instrument = ChoiceItem()
+
     name = models.CharField(max_length=64, unique=True)
     normalized_name = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length=64, choices=classification.choices,)
 
     def __str__(self):
         return self.name
 
+class InstrumentCondition(DjangoChoices):
+        broken = ChoiceItem()
+        needs_repair_soon = ChoiceItem()
+        decent_condition = ChoiceItem()
+        excellent_condition = ChoiceItem()
+        in_repair_shop = ChoiceItem()
 
 
-class UserPersona(models.Model):
-    name = models.CharField(max_length=64, unique=True)
-    normalized_name = models.CharField(max_length=64, unique=True)
-    description = models.CharField(max_length=200)
+class Instrument(models.Model):
+    locker_number = models.OneToOneField(Locker, on_delete=models.SET_NULL, blank=True, null=True)    # details
+
+    student_name = models.CharField(max_length=64, default="Not assigned")
+
+    instrument_name = models.CharField(max_length=64, blank=True, null=True)
+    serial_number = models.CharField(max_length=64, unique=True, blank=True, null=True)
+
+    condition = models.CharField(max_length=64, choices=InstrumentCondition.choices, blank=True, null=True)
+    #Woodwind items
+    mouth_piece = models.BooleanField(default=True)
+    ligature = models.BooleanField(default=True)
+    cork_grease = models.BooleanField(default=True)
+    reeds = models.BooleanField(default=True)
+    case = models.BooleanField(default=True)
+
+    notes = models.CharField(max_length=500, blank=True, null=True)
+
+    instrument_category = models.ForeignKey(
+        InstrumentCategory, on_delete=models.SET_NULL, blank=True, null=True)
+
 
     def __str__(self):
-        return self.name
-
-
-class UserProfile(models.Model):
-    # owner
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-
-    # settings
-    is_full_name_displayed = models.BooleanField(default=True)
-
-    # details
-    bio = models.CharField(max_length=500, blank=True, null=True)
-    website = models.URLField(max_length=200, blank=True, null=True)
-    persona = models.ForeignKey(
-        UserPersona, on_delete=models.SET_NULL, blank=True, null=True)
-    interests = models.ManyToManyField(UserInterest, blank=True)
+            return self.student_name
