7065bbe0a66c2990591e8d2359757398fcc6c523
Peter <ruecktenwald@gmail.com>
2022-09-04 20:28:52 -0400

    lock model

--- instrument_inventory_app/apps/inventory/admin.py
+++ instrument_inventory_app/apps/inventory/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Instrument, InstrumentKind, Locker, Student
+from .models import Instrument, InstrumentKind, Locker, Student, Lock
 
 admin.site.register(Instrument)
 admin.site.register(Locker)
+admin.site.register(Lock)
 admin.site.register(Student)
